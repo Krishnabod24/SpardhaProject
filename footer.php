7df5e0d9ca834246efc4374c31c6e980ba759b9f
Krishnabod24 <34591534+Krishnabod24@users.noreply.github.com>
2018-12-13 23:44:10 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,43 @@
+<!-- Footer -->
+<footer>
+
+    <!-- Footer Elements -->
+    <div>
+<center>
+      <!-- Grid row-->
+            <!-- Facebook -->
+            <a class="fb-ic">
+              <i class="fa fa-facebook fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!-- Twitter -->
+            <a class="tw-ic">
+              <i class="fa fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!-- Google +-->
+            <a class="gplus-ic">
+              <i class="fa fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!--Linkedin -->
+            <a class="li-ic">
+              <i class="fa fa-linkedin fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!--Instagram-->
+            <a class="ins-ic">
+              <i class="fa fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
+            </a>
+            <!--Pinterest-->
+            <a class="pin-ic">
+              <i class="fa fa-pinterest fa-lg white-text fa-2x"> </i>
+            </a>
+			</center>
+    </div><br>
+    <!-- Footer Elements -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="C:\wamp\www\MyProject\mainpage.php"> EBOOK XPRESS</a>
+    </div>
+    <!-- Copyright -->
+
+  </footer>
+  <!-- Footer -->
